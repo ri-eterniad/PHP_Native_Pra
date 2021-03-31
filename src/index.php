516274339e0252de907863bd99b3a9d05c2104f9
YoshihiroIshizka <mdmdmdmdmd57@gmail.com>
2021-03-31 18:55:32 +0900

    phpの練習、SQL DB接続、PDOなど

--- src/index.php
+++ src/index.php
@@ -1,114 +1,76 @@
-<?php 
+<?php
+class Post {
+    //vagrantのサーバー
+    private $DB_HOST ="192.168.33.12";
+    private $DB_NAME ="php_yoshi";
+    private $DB_USER ="yoshi";
+    private $DB_PASSWORD ="phpHacks2021@";
 
-echo "やあ、世界";
-echo "<br>";
+    //DBへの接続関数
+    protected function db_access(){
+        //PHPがエラーを吐くようにする
+        error_reporting(E_ALL & ~E_NOTICE);
 
-//変数
-$baseball =42;
-$soccer =20;
-$backetball =60;
-$music =50;
-
-echo "音楽の部員は".$music."人です。";
-echo "<br>";
-
-//配列
-$data1=array(
-    "name" => "Kazu" ,
-    "sex" => 1);
-$data2=array(
-    "name" => "Kazumasa",
-    "sex" => 1);
-$data3=array(
-    "name" => "Mina",
-    "sex" => 0);
-
-$friends =array($data1,$data2,$data3);
-
-foreach($friends as $friend){
-    
-    if($friend["sex"] == 0){
-        echo $friend["name"]."は女性です";
-    }else{
-        echo $friend["name"]."は男性です";
+        //データベースへの接続
+        //公式から雛形持ってくる
+        try {
+            $dbh = new PDO('mysql:host='.$this->DB_HOST.';dbname='.$this->DB_NAME, $this->DB_USER, $this->DB_PASSWORD);
+            //あくまで接続は別で記述する
+            return $dbh;
+        } catch (PDOException $e) {
+            echo "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
     }
+
+    public function index(){
+        $dbh = $this->db_access();
+
+        $sql ="SELECT * FROM posts";
+        //準備段階
+        $stmt = $dbh->prepare($sql);
+
+        //実行
+        $stmt->execute();
+        $result =$stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
 }
+
+$model = new Post();
+$result = $model->index();
+
+$posts = $result;
+// var_dump($posts[0]);
 ?>
-HTMLとして書く
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>連想配列if,foreach文の基本</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" 
+      rel="stylesheet" 
+      integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" 
+      crossorigin="anonymous">
+    <title>TODOリスト</title>
 </head>
 <body>
-<p>↑上記のphpのecho文は練習なので気にしないで</p>
-<?php foreach($friends as $friend):?>
-    <div>
-        <div>
-            <h1><?php echo "名前:".$friend["name"] ;?></h1>
-        </div>
-        <?php if($friend["sex"]==0):?>
-        <div>
-            <p>女性です</p>
-        </div>
-        <?php else :?>
-        <div>
-            <p>男性です</p>
-        </div>
-        <?php endif;?>
+    <div class="container m-4">
+        <h1>TODOリスト</h1>
     </div>
-<?php endforeach;?>
+    <?php foreach($posts as $post):?>
+    <div class="container m-4">
+        <div>
+            <h2><?php echo $post['title'] ;?></h2>
+        </div>
+        <div>
+            <p>詳細：<?php echo $post['body'] ;?></p>
+        </div>
+    </div>
+    <?php endforeach;?>
+    
 </body>
 </html>
-
-<?php
-//関数
-//4試合の平均均得点を知りたい 
-function Ave($a,$b,$c,$d){
-    return ($a + $b + $c + $d)/4;
-}
-
-$myAve =ave(67,34,72,90);
-
-echo "平均得点は".$myAve."点です。";
-
-//クラス
-
-class Persona{
-    protected $name ="名前"; 
-    protected $hobby ="趣味"; 
-    protected $age ="年齢"; 
-
-    //メソッド
-    public function __construct($name,$hobby,$age){
-        $this->name = $name;
-        $this->hobby = $hobby;
-        $this->age = $age;
-    }
-
-    public function getName(){
-        return $this->name;
-    }
-
-    public function getHobby(){
-        return $this->hobby;
-    }
-
-    public function getAge(){
-        return $this->age;
-    }
-}
-
-$koki = new Persona("Koki","バスケ",13);
-$name =$koki->getName();
-$hobby =$koki->getHobby();
-$age =$koki->getAge();
-
-echo "<br>";
-
-echo "私は".$name."。趣味は".$hobby."。".$age."才よよろしく。";
-
-?>
